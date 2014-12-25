ee673deadb056082b088615a6e8ff8f773d8a4ed
Kenta OONO <k.oono.delta@gmail.com>
2014-12-26 08:45:04 +0900

    Build pass

--- NN/Network.hs
+++ NN/Network.hs
@@ -8,42 +8,31 @@ type Blob = [Float]
 data Network d1 d2 = Nw (d1 -> d2)
 
 class INetwork n where
+    fromFunc :: (d1 -> d2) -> n d1 d2
     forward :: n d1 d2 -> d1 -> d2
     (.) :: n d2 d3 -> n d1 d2 -> n d1 d3
-    (|+|) :: (Num d3) => n d1 d3 -> n d2 d3 -> n (d1, d2) d3
+    (|+|) :: n d1 d3 -> n d2 d4 -> n (d1, d2) (d3, d4)
 
 instance INetwork Network where
+    fromFunc f = Nw f
     forward (Nw f) = f
-    (Nw g) . (Nw f) = Nw (g Prelude.. f)
-    (Nw f) |+| (Nw g) = Nw (\(x, y) -> f x + g y)
+    (Nw g) . (Nw f) = fmap g (Nw f)
+    (Nw f) |+| (Nw g) = Nw (\(x, y) -> (f x, g y))
 
-instance (Num y) => Num (x -> y) where
-    f + g = \x -> f x + g x
-    f * g = \x -> f x * g x
-    abs f = abs Prelude.. f
-    fromInteger n = \_ -> fromInteger n
-    signum f = signum Prelude.. f
-    negate f = negate Prelude.. f
-
-instance (Num d2) => Num (Network d1 d2) where
-    (Nw f) + (Nw g) = Nw (f + g)
-    (Nw f) * (Nw g) = Nw (f * g)
-    abs (Nw f) = Nw (abs f)
-    fromInteger n = Nw (fromInteger n)
-    signum (Nw f) = Nw (signum f)
-    negate (Nw f) = Nw (negate f)
-
--- instance (A.Applicative a, Num x) => Num a x where
---      as + bs = A.liftA2 (+) as bs
---      as * bs = A.liftA2 (*) as bs
---      abs as = A.liftA abs as
---      fromInteger n = A.pure $ fromInteger n
---      signum as = A.liftA signum as
---      negate as = A.liftA negate as
+instance Functor (Network n) where
+    fmap g (Nw f) = Nw $ g Prelude.. f
 
 fc :: (Num a) => M.Matrix a -> Network (M.Matrix a) (M.Matrix a)
-fc w = Nw((*) w)
+fc w = Nw $ (*) w
 
 lift :: (A.Applicative a) => Network d1 d2 -> Network (a d1) (a d2)
-lift (Nw f) = Nw (A.liftA f)
+lift (Nw f) = Nw $ A.liftA f
 
+elementWise :: (A.Applicative a) => (d1 -> d2) -> Network (a d1) (a d2)
+elementWise f = lift $ Nw f
+
+elementWiseAdd :: (Num d1) => Network (d1, d1) d1
+elementWiseAdd = Nw $ \(x, y) -> (x Prelude.+ y)
+
+(+) :: (Num d3) => Network d1 d3 -> Network d2 d3 -> Network (d1, d2) d3
+(Nw f) + (Nw g) = elementWiseAdd NN.Network.. ((Nw f) |+| (Nw g))
--- app/Sample.hs
+++ app/Sample.hs
@@ -7,9 +7,10 @@ import qualified Data.Vector as V
 n = N.Nw (fmap (+1)) :: N.Network (M.Matrix Float) (M.Matrix Float)
 m = N.Nw (fmap (*2)) :: N.Network (M.Matrix Float) (M.Matrix Float)
 l = m N.. n
-p = m + n
+p = m N.|+| n
 w = M.matrix 4 3  (\(i, j) -> i+j*2)
 q = N.fc w
+r = m N.+ n
 
 -- main = putStrLn $ show $ N.forward p [2, 3]
 main = putStrLn $ show $ N.forward q $ M.fromList 3 1 [1, 2, 3]
