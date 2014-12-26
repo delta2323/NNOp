754c24564ba3b98426ab308ead0c7e6054f6d997
Kenta OONO <k.oono.delta@gmail.com>
2014-12-26 23:56:15 +0900

    Add examples

--- app/Sample.hs
+++ app/Sample.hs
@@ -8,9 +8,13 @@ n = N.Nw (fmap (+1)) :: N.Network (M.Matrix Float) (M.Matrix Float)
 m = N.Nw (fmap (*2)) :: N.Network (M.Matrix Float) (M.Matrix Float)
 l = m N.. n
 p = m N.|+| n
-w = M.matrix 4 3  (\(i, j) -> i+j*2)
+w = M.matrix 4 3 (\(i, j) -> fromIntegral $ i+j*2)
 q = N.fc w
 r = m N.+ n
 
--- main = putStrLn $ show $ N.forward p [2, 3]
-main = putStrLn $ show $ N.forward q $ M.fromList 3 1 [1, 2, 3]
+x1 = M.fromList 3 1 [1, 2, 3]
+x2 = M.fromList 3 1 [1, 2, 3]
+
+main = (putStrLn $ show $ N.forward l $ x1)
+  >> (putStrLn $ show $ N.forward p $ (x1, x2))
+  >> (putStrLn $ show $ N.forward q $ x1)
